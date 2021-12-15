ab9e4ddabf70ba8ff66373693923183ce386c8a6
Владислав Кравец <vlad.kravets12@gmail.com>
2021-12-15 15:32:54 +0200

    release beta

---
+++ Ierarhiya/ElectricalAppliance.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+public abstract class WorksTool
+{ // работа инструмента
+    public abstract void start();
+
+    public void work()
+    {
+        Console.WriteLine("Инструмент выполняет работу");
+    }
+    
+    public abstract void end();
+}
+
+public interface RequirementsTools // требования к инструменту
+{
+    String getName();
+    String getModel();
+}
+
+public class ElectricInstrument : WorksTool
+{
+    private String name;
+    private String model;
+    private bool active;
+
+    protected ElectricInstrument(string name, string model, bool active) // конструктор
+    {
+        this.name = name;
+        this.model = model;
+        this.active = active;
+    }
+    
+    public ElectricInstrument() // пустой
+    {
+        name = null;
+        model = null;
+        active = false;
+    }
+    
+    ~ElectricInstrument() // деструктор
+    {
+        Console.WriteLine("Объект уничтожен");
+    }
+
+    public string Name => name;
+
+    public string Model => model;
+
+    public bool Active => active;
+
+    public override void start() // абстрактные метод старта
+    {
+        if (!active)
+        {
+            Console.WriteLine("Електро інструмент " + name + " " + model + " почав виконувати дію");
+            active = true;
+        }
+        else
+        {
+            Console.WriteLine("Електро інструмент уже виконує дію");
+        }
+    }
+
+    public override void end()
+    {
+        if (active)
+        {
+            Console.WriteLine("Електро інструмент " + name + " " + model + " закінчив виконувати дію");
+            active = false;
+        }
+        else
+        {
+            Console.WriteLine("Електро інструмент ще не виконує дію");
+        }
+    }
+}
+
+public class KitchenInstrumentsSaveProducts : ElectricInstrument // для сохранения продуктов
+{
+    private int minTemp, maxTemp;
+    private String type;
+    private int capacity;
+    private bool freezer;
+    
+    public KitchenInstrumentsSaveProducts(string name, string model, bool active, 
+        int minTemp, int maxTemp, string type, int capacity,bool freezer) : base(name, model, active)
+    {
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        this.type = type;
+        this.capacity = capacity;
+        this.freezer = freezer; 
+    }
+
+    public int MinTemp => minTemp;
+
+    public int MaxTemp => maxTemp;
+
+    public string Type => type;
+
+    public int Capacity => capacity;
+
+    public bool Freezer => freezer;
+}
+
+// public class Refrigerator : KitchenInstrumentsSaveProducts
+// {
+//     
+//     public Refrigerator(string name, string model, bool active, int minTemp, int maxTemp, string type, int capacity) : base(name, model, active, minTemp, maxTemp, type, capacity)
+//     {
+//         
+//     }
+// }
+
+public class KitchenInstrumentsMechanicalRestoration : ElectricInstrument // для механической обработки
+{
+    private int minSpeed, maxSpeed;
+    private bool portable; 
+    private String typeNozzle; // тип насадки
+
+    public KitchenInstrumentsMechanicalRestoration(string name, string model, bool active, int minSpeed, int maxSpeed, bool portable, string typeNozzle) : base(name, model, active)
+    {
+        this.minSpeed = minSpeed;
+        this.maxSpeed = maxSpeed;
+        this.portable = portable;
+        this.typeNozzle = typeNozzle;
+    }
+}
---
+++ Ierarhiya/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Ierarhiya
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Вы хотите создать предмет: " +
+                              "\n1. Бытовой предмет для сохранения продуктов" +
+                              "\n2. Бытовой предмет для механической обработки продуктов");
+
+            string inp = Convert.ToString(Console.ReadLine());
+            switch (inp)
+            {
+                case "1":
+                {
+                    Console.WriteLine("Введите название продукта: ");
+                    string name = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите модель: ");
+                    string model = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите минимальная температура: ");
+                    int minTemp = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Введите максимальную температуру: ");
+                    int maxTemp = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Введите тип продукта: ");
+                    string type = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите вместимость: ");
+                    int capacity = Convert.ToInt16(Console.ReadLine());
+                    
+                    KitchenInstrumentsSaveProducts kitchenInstrumentsSaveProducts = new KitchenInstrumentsSaveProducts(name,model,false,minTemp,maxTemp,type,capacity,false);
+
+                    kitchenInstrumentsSaveProducts.start();
+                    kitchenInstrumentsSaveProducts.work();
+                    kitchenInstrumentsSaveProducts.end();
+                    break;
+                }
+                case "2":
+                {
+                    Console.WriteLine("Введите название продукта: ");
+                    string name = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите модель: ");
+                    string model = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите минимальная скорость: ");
+                    int minSpeed = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Введите максимальную скорость: ");
+                    int maxSpeed = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Введите портативный ли продукт: ");
+                    bool portable = Convert.ToBoolean(Console.ReadLine());
+                    Console.WriteLine("Введите тип насадок: ");
+                    string typeNozzle = Convert.ToString(Console.ReadLine());
+                    
+                    var kitchenInstrumentsSaveProducts = 
+                        new KitchenInstrumentsMechanicalRestoration(name,model,false,minSpeed,maxSpeed,portable,typeNozzle);
+
+                    kitchenInstrumentsSaveProducts.start();
+                    kitchenInstrumentsSaveProducts.work();
+                    kitchenInstrumentsSaveProducts.end();
+                    break;
+                }
+            }
+        }
+    }
+}
